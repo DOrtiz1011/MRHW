358904a3e266a68fb9b637cd5083a1a277a4797d
Daniel Ortiz <DOrtiz1011@gmail.com>
2016-06-05 15:37:17 -0400

    All 4 list functional
    List 1-3 are working with CSS but list 4 is not picking it up.

--- AdDataSite/Controllers/AdDataController.cs
+++ AdDataSite/Controllers/AdDataController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 using AdDataSite.AdDataService;
+using AdDataSite.Models;
 
 namespace AdDataSite.Controllers
 {
@@ -21,13 +22,53 @@ namespace AdDataSite.Controllers
         public ActionResult List1_AllAds()
         {
             ViewBag.title = "All Ads";
-            return View("AllColsTable", GetAdData().OrderBy(x => x.Brand.BrandName).ToList());
+            var adData = GetAdData().OrderBy(x => x.Brand.BrandName).ToList();
+
+            return View("AllColsAdsTable", adData);
         }
 
         public ActionResult List2_CoverAds()
         {
-            ViewBag.title = "All Cover";
-            return View("AllColsTable", GetAdData().Where(x => x.Position == "Cover" && x.NumPages >= 0.5m).OrderBy(x => x.Brand.BrandName).ToList());
+            ViewBag.title = "All Cover Ads";
+            var adData = GetAdData().Where(x => x.Position == "Cover" && x.NumPages >= 0.5m).OrderBy(x => x.Brand.BrandName).ToList();
+
+            return View("AllColsAdsTable", adData);
+        }
+
+        public ActionResult List3_Top5AdsByCoverageAndBrand()
+        {
+            ViewBag.title = "Top 5 Ads By Coverage and Brand";
+
+            var adData = GetAdData().GroupBy(x => new { numPages = x.NumPages, brand = x.Brand.BrandName })
+                .OrderByDescending(x => x.Key.numPages)
+                .ThenBy(x => x.Key.brand)
+                .Take(5)
+                .Select(x => x.First())
+                .ToList();
+
+            return View("AllColsAdsTable", adData);
+        }
+
+        public ActionResult List4_TopBrandsByCoverage()
+        {
+            ViewBag.title = "Top 5 Brands by Coverage Amount";
+
+            var adData = GetAdData()
+                .GroupBy(ad => ad.Brand.BrandName)
+                .Select(x => new { brand = x.Key, totalCoverage = x.Sum(g => g.NumPages) })
+                .OrderByDescending(g => g.totalCoverage)
+                .ThenBy(g => g.brand)
+                .Select(g => new { g.brand, g.totalCoverage })
+                .Take(5);
+
+            var BrandViewModelList = new List<BrandViewModel>();
+
+            foreach (var a in adData)
+            {
+                BrandViewModelList.Add(new BrandViewModel() { BrandName = a.brand, TotalCoverage = a.totalCoverage });
+            }
+
+            return PartialView("AllColsBrandsTable", BrandViewModelList);
         }
     }
 }
---
+++ AdDataSite/Models/BrandViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+
+namespace AdDataSite.Models
+{
+    public class BrandViewModel : IExtensibleDataObject, INotifyPropertyChanged
+    {
+        public string BrandName { get; set; }
+        public decimal TotalCoverage { get; set; }
+
+        [NonSerialized()]
+        private ExtensionDataObject extensionDataField;
+
+        [Browsable(false)]
+        public ExtensionDataObject ExtensionData
+        {
+            get
+            {
+                return extensionDataField;
+            }
+            set
+            {
+                extensionDataField = value;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler propertyChanged = this.PropertyChanged;
+            if ((propertyChanged != null))
+            {
+                propertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
